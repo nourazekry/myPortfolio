bdbaf5bc3e70762951d62d4c47aad84db2a78489
nourazekry <nourazekry2001@gmail.com>
2024-04-02 14:54:34 -0400

    styling home page

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,20 +1,18 @@
 import Image from "next/image";
 import Link from 'next/link';
-import styles from './Home.module.css'; // Assuming you have a CSS file named Home.module.css
+import styles from './Home.module.css'; 
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <nav className={styles.toolbar}>
-        <Link href="/projects">
-          <a className={styles.toolbarLink}>Projects</a>
-        </Link>
-        <Link href="/experiments">
-          <a className={styles.toolbarLink}>Experiments</a>
-        </Link>
+        <Link href="/experience" className={styles.toolbarLink}>Experience</Link>
+        <Link href="/projects" className={styles.toolbarLink}>Projects</Link>
+        <Link href="/experiments" className={styles.toolbarLink}>Experiments</Link>
+        <Link href="/contact" className={styles.toolbarLink}>Contact</Link>
       </nav>
       <div className={styles.content}>
-        <h1 className={styles.name}>Your Name</h1>
+        <h1 className={styles.name}>Noura Zekry</h1>
         <p className={styles.description}>Your Description</p>
       </div>
     </div>
